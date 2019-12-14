ffc46432f065f51dbbae14889d88d4ad43f75afd
levdjepko <levdjepko@gmail.com>
2019-12-13 16:00:59 -0800

    Initial version uploaded
    So far only calculates based on 2019 as current year

---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeartRateApp
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HeartRateCalculator calculateMaxRate = new HeartRateCalculator(textBox1.Text, Convert.ToInt32(textBox2.Text), 2019);
+            calculateMaxRate.CalculateMaxRate(calculateMaxRate.Age);
+            label4.Text = $"{calculateMaxRate.Name}, your max heart rate is:";
+            label5.Text = calculateMaxRate.MaxRate.ToString();
+            label6.Text = calculateMaxRate.LowRange.ToString();
+            label7.Text = calculateMaxRate.HighRange.ToString();
+        }
+    }
+}
---
+++ HeartRateCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeartRateApp
+{
+    class HeartRateCalculator
+    {
+        public string Name { get; set; }
+        public int BirthYear {get; set;}
+        public int CurrentYear { get; set; }
+        public int Age { get; set; }
+        public int MaxRate { get; set; }
+        public int LowRange { get; set; }
+        public int HighRange { get; set; }
+
+        public HeartRateCalculator(string name, int birthYear, int currentYear)
+        {
+            Name = name;
+
+            if (birthYear < currentYear && birthYear > 1900)
+            {
+                BirthYear = birthYear;
+            }
+
+            CurrentYear = currentYear;
+
+            Age = currentYear - birthYear;
+        }
+
+        public void CalculateMaxRate (int Age)
+        {
+            MaxRate = 220 - Age;
+            LowRange = MaxRate * 50 / 100;
+            HighRange = MaxRate * 85 / 100;       
+        }
+    }
+}
